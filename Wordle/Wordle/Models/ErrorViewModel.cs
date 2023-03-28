0913152c1595560ae21596d885c5bbcc267f70ba
Bartłomiej Całus <bartlomiej.calus01@gmail.com>
2023-03-28 21:46:28 +0200

    init

---
+++ Wordle/Wordle/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Wordle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
