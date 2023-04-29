50b5978b58b4199a53a8f928aecbba7dfb1ecf5c
Kamil Maciejczyk <kamil.maciejczyk@accenture.com>
2023-04-29 12:55:02 +0200

    added dictionary request

--- Wordle/Wordle/Models/ArrayRequest/WordsArray.cs
+++ Wordle/Wordle/Models/ArrayRequest/WordsArray.cs
@@ -1,42 +1,70 @@
-﻿using Microsoft.DotNet.Scaffolding.Shared.CodeModifier.CodeChange;
-using Newtonsoft.Json.Linq;
-using System;
-using System.IO;
-using System.Data;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json.Linq;
 using RestSharp;
-using Newtonsoft.Json.Linq;
-using System.Security.Cryptography.X509Certificates;
-using Method = RestSharp.Method;
+using System;
+using System.Threading.Tasks;
 
-namespace Wordle.Models.ArrayRequest
+namespace MyNamespace
 {
-    public class WordsArray
-{
-    public async Task<string[]> GetWordsArray()
+    public class WordsApi
     {
-        var client = new RestClient("https://random-word-api.herokuapp.com");
-        var request = new RestRequest("/all", Method.Get);
-
-        RestResponse restResponse = await client.GetAsync(request);
-        string responseContent = restResponse.Content;
-
-        JArray jsonArray = JArray.Parse(responseContent);
-        string[] wordsArray = jsonArray.ToObject<string[]>();
-
-        return wordsArray;
-    }
-
-    public async Task Main()
-    {
-        string[] wordsArray = await GetWordsArray();
-
-        Console.WriteLine("Zwrócone słowa:");
-        foreach (string word in wordsArray)
+        public async Task<string[]> GetRandomWords()
         {
-            Console.WriteLine(word);
+            var client = new RestClient("https://random-word-api.herokuapp.com");
+            var request = new RestRequest("/word", Method.Get);
+
+            var response = await client.ExecuteAsync(request);
+            var words = JArray.Parse(response.Content).ToObject<string[]>();
+
+            return words;
         }
     }
-}
 
+    public class DictionaryApi
+    {
+        public async Task<string> GetDefinition(string word)
+        {
+            var client = new RestClient("https://api.dictionaryapi.dev");
+            var request = new RestRequest($"/api/v2/entries/en/{word}", Method.Get);
+
+            var response = await client.ExecuteAsync(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return "word does not exist in diciotionary API";
+            }
+
+            var responseContent = JArray.Parse(response.Content);
+
+            if (responseContent[0]["meanings"][0]["definitions"][0]["definition"] != null)
+            {
+                var definition = (string)responseContent[0]["meanings"][0]["definitions"][0]["definition"];
+                return definition;
+            }
+            else
+            {
+                return "No definitions found";
+            }
+        }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var wordsApi = new WordsApi();
+            var words = await wordsApi.GetRandomWords();
+
+            Console.WriteLine("Słowo:");
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+
+                var dictionaryApi = new DictionaryApi();
+                var definition = await dictionaryApi.GetDefinition(word);
+
+                Console.WriteLine("\nDefinicja słowa:");
+                Console.WriteLine(definition);
+            }
+        }
+    }
 }
